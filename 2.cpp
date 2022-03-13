33230a78b9eb31ebaae808618357cf2b6d1ea206
zc136 <101462068+zc136@users.noreply.github.com>
2022-03-13 12:47:06 +0800

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+ int a =0;
+ int b =0;
+ int c =0;
+ scanf("%d%d",&a,&b);
+ c=a-b;
+ printf("c=%d\n",c);
+	
+	return 0;
+ } 
---
+++ 4.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+{	
+	int a=10;
+	int b=2;
+	int max=0;
+	max=(a>b?a:b);
+	printf("max=%d\n",max);
+	//if(a>b)
+	//max=a;
+	//else
+    //	max=b;		
+     //printf("max=%d\n",max);	
+	return 0;
+ } 
