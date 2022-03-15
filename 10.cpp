15f2127e2ee39100f152c4aa1c696a57fcef3588
张闯 <101462068+zc136@users.noreply.github.com>
2022-03-15 12:51:39 +0800

    Add files via upload

---
+++ 10.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+	int day=0;
+	scanf("%d",&day);
+	switch(day)
+	{
+		case 1:
+			printf("星期一\n");
+			//break;            (break语句用来打破语句)
+		case 2:
+		    printf("星期二\n");
+
+		    //break;
+		case 3:
+		    printf("星期三\n");
+		    //break;
+		case 4:
+		     printf("星期四\n");
+		    // break; 
+	    case 5:
+		     printf("星期五\n");
+		     //break;
+		case 6:
+		     printf("星期六\n");
+		    // break;
+		case 7:
+		     printf("星期日\n");
+			// break;	
+	}
+		return 0;
+ } 
