cd6b35809d10bd43c391e681099543529a5f1dce
张闯 <101462068+zc136@users.noreply.github.com>
2022-03-21 13:14:52 +0800

    Add files via upload

---
+++ 23.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<math.h>
+int Facl(int n)
+{
+	int i=0;
+	int ret=1;
+	for(i=1;i<=n;i++)
+	{
+		ret=ret*i;
+	}
+	return ret;
+}
+int main()
+{
+	int n=0;
+	int ret=0;
+	scanf("%d",&n);
+	ret=Facl(n);
+	printf("%d\n",ret);
+	return 0;
+}
