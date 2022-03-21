ad0f3f9127b7492f086c89e4b66d352ccedbd9aa
张闯 <101462068+zc136@users.noreply.github.com>
2022-03-21 14:08:06 +0800

    Add files via upload

---
+++ 25.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+int arr[3][4]={{1,2,3},{4,5}};
+int i=0;
+for (i=0;i<3;i++)
+{
+	int j=0;
+	for(j=0;j<3;j++)
+	{
+		printf("%d",arr[i][j]);
+	}
+	printf("\n");
+}
+return 0;
+}
