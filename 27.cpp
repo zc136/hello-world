f814f734855b3c37d9f9f8078bf0fd4ed9486bfa
张闯 <101462068+zc136@users.noreply.github.com>
2022-03-22 17:29:44 +0800

    Add files via upload

---
+++ 27.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/*int main()
+{
+	int a=16;
+	//>> 表示右移操作符
+	//移动的是二进制位 
+	int b=a>>2;
+	printf("b=%d\n",b);
+	return 0;
+}*/
+/*int main()
+{
+	//&按照二进制位与
+	int a=3;
+	int b=5;
+	int c=a&b;
+	printf("%d\n",c);
+	return 0; 
+}*/
+int main()
+{
+	int a=3;
+	int b=5;
+	int c=a|b;
+	printf("%d\n",c);
+	return 0; 
+}
