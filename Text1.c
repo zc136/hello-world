0c18760c4fc1401027d107b60cfa5be97be042e6
张闯 <1362776605@qq.com>
2022-05-14 18:15:38 +0800

    Add files via upload

--- Text1.c
+++ Text1.c
@@ -1,49 +1,53 @@
 #include<reg51.h>
-unsigned char code Tab[10]={0xc0,0xf9,0xa4,0xb0,0x99,0x92,0x82,0xf8,0x80,0x90}; 
-unsigned char int_time;
-unsigned char second;
+unsigned char ID;
+sbit S1=P1^4;
 void delay(void)
 {
-  unsigned char i;
-  for(i=0;i<200;i++)
-        ;
+unsigned char i,j;
+for(i=0;i<200;i++)
+for(j=0;j<100;j++)
+;
 }
- void DisplaySecond(unsigned char k)
+void main(void)
 {
-   P2=0xfe;
-   P0=Tab[k/10];
-   delay();
-   P2=0xfd;
-   P0=Tab[k%10];
-   delay();
+TMOD=0x20;
+EA=1;
+ET1=1;
+TR1=1;
+TH1=256-200;
+TL1=256-200;
+ID=0;
+while(1)
+{
+switch(ID)
+{
+case 0:
+P3=0xfe;
+break;
+case 1:
+P3=0xfd;
+break;
+case 2:
+P3=0xfb;
+break;
+case 3:
+P3=0xf7;
+break;
 }
-  void main(void)
-   {
-	  TMOD=0x01;
-	  TH0=(65536-46083)/256;
-	   TL0=(65536-46083)%256;
-		EA=1;
-		ET0=1;
-		TR0=1;
-		int_time=0;
-		second=0;		
-        while(1)
-		{
-	       DisplaySecond(second);
-		 }
-    }  
-  void interserve(void ) interrupt 1 using 1  
-  {
-    TR0=0;
-    int_time ++;	
-	 if(int_time==20)
-	  {
-        int_time=0;
-	    second++;
-	    if(second==60)
-	      second =0;
-	  }		      
-  	TH0=(65536-46083)/256;
-	TL0=(65536-46083)%256;
-	TR0=1;
 }
+}
+void intersev(void) interrupt 3 using 1
+{
+TR1=0;
+P1=0xff;
+if(S1==0)
+{
+delay();
+if(S1==0)
+ID=ID+1;
+}
+if(ID==4)
+ID=0;
+TR1=1;
+}
+
