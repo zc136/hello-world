55cbebbbf1581f36942325ef25c4bdb4b4db396e
张闯 <1362776605@qq.com>
2022-05-22 18:22:09 +0800

    Add files via upload

--- Text1.c
+++ Text1.c
@@ -1,34 +1,49 @@
 #include<reg51.h>
-unsigned char code Tab[]={0xfe,0xfd,0xfb,0xf7,0xef,0xdf,0xbf,0x7f};
-void Send(unsigned char dat)
-{
-SBUF=dat;
-while(TI==0)
-;
-TI=0;
-}
+unsigned char code Tab[10]={0xc0,0xf9,0xa4,0xb0,0x99,0x92,0x82,0xf8,0x80,0x90}; 
+unsigned char int_time;
+unsigned char second;
 void delay(void)
 {
-unsigned char m,n;
-for(m=0;m<200;m++)
-for(n=0;n<250;n++)
-;
+  unsigned char i;
+  for(i=0;i<200;i++)
+        ;
 }
-void main(void)
+ void DisplaySecond(unsigned char k)
 {
-unsigned char i;
-TMOD=0x20;
-SCON=0x40;
-PCON=0x00;
-TH1=0xfd;
-TL1=0xfd;
-TR1=1;
-while(1)
-{
-for(i=0;i<8;i++)
-{
-Send(Tab[i]);
-delay();
+   P2=0xfe;
+   P0=Tab[k/10];
+   delay();
+   P2=0xfd;
+   P0=Tab[k%10];
+   delay();
 }
-}
+  void main(void)
+   {
+	  TMOD=0x01;
+	  TH0=(65536-46083)/256;
+	   TL0=(65536-46083)%256;
+		EA=1;
+		ET0=1;
+		TR0=1;
+		int_time=0;
+		second=0;		
+        while(1)
+		{
+	       DisplaySecond(second);
+		 }
+    }  
+  void interserve(void ) interrupt 1 using 1  
+  {
+    TR0=0;
+    int_time ++;	
+	 if(int_time==5)
+	  {
+        int_time=0;
+	    second++;
+	    if(second==60)
+	      second =0;
+	  }		      
+  	TH0=(65536-46083)/256;
+	TL0=(65536-46083)%256;
+	TR0=1;
 }
