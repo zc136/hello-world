ac3c64735d0e3b2ba0b1f8e5cbb5c0b9caff2db5
张闯 <1362776605@qq.com>
2022-05-30 10:45:57 +0800

    Add files via upload

--- Text1.c
+++ Text1.c
@@ -1,110 +1,39 @@
 #include<reg51.h>
-sbit S1=P1^4;
-sbit S2=P1^5;
-sbit S3=P1^6;
-sbit S4=P1^7;
-unsigned char keyval;
-void led_delay(void)
+void delay(void)
 {
 unsigned char i,j;
-for(i=0;i<250;i++)
-for(j=0;j<250;j++)
+for(i=0;i<20;i++)
+for(j=0;j<20;j++)
 ;
-}
-void delay20ms(void)
-{
-unsigned char i,j;
-for(i=0;i<100;i++)
-for(j=0;j<60;j++)
-;
-}
-void forward(void)
-{
-P3=0xfe;
-led_delay();
-P3=0xfd;
-led_delay();
-P3=0xfb;
-led_delay();
-P3=0xf7;
-led_delay();
-P3=0xef;
-led_delay();
-P3=0xdf;
-led_delay();
-P3=0xdf;
-led_delay();
-P3=0x7f;
-led_delay();
-P3=0xfe;
-led_delay();
-}
-void backward(void)
-{
-P3=0x7f;
-led_delay();
-P3=0xbf;
-led_delay();
-P3=0xdf;
-led_delay();
-P3=0xef;
-led_delay();
-P3=0xf7;
-led_delay();
-P3=0xfb;
-led_delay();
-P3=0xfd;
-led_delay();
-P3=0xfe;
-led_delay();
-}
-void stop(void)
-{
-P3=0xff;
-}
-void flash(void)
-{
-P3=0xff;
-led_delay();
-P3=0x00;
-led_delay();
-}
-void key_scan(void)
-{
-if((P1&0xf0)!=0xf0)
-{
-delay20ms();
-if(S1==0)
-keyval=1;
-if(S2==0)
-keyval=2;
-if(S3==0)
-keyval=3;
-if(S4==0)
-keyval=4;
-}
 }
 void main(void)
 {
-keyval=0;
 while(1)
 {
-key_scan();
-switch(keyval)
-{
-case 1:forward();
-break;
-case 2:backward();
-break;
-case 3:stop();
-break;
-case 4:flash();
-break;
+P2=0x01;
+P0=0xc0;
+delay();
+P2=0x02;
+P0=0x92;
+delay();
+P2=0x04;
+P0=0xb0;
+delay();
+P2=0x08;
+P0=0x99;
+delay();
+P2=0x10;
+P0=0xc0;
+delay();
+P2=0x20;
+P0=0xf9;
+delay();
+P2=0x40;
+P0=0xb0;
+delay();
+P2=0x80;
+P0=0xa4;
+delay();
+//P2=0xff;
 }
 }
-}
-
-
-
-
-
