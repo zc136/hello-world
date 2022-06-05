66f0c9e690987c6cba1ba25222deaa1975f5c3b2
张闯 <1362776605@qq.com>
2022-06-05 09:02:28 +0800

    Add files via upload

--- Text1.c
+++ Text1.c
@@ -1,24 +1,33 @@
 #include<reg51.h>
-unsigned char Receive(void)
+unsigned char code Tab[]={0xFE,0xFD,0xFB,0xF7,0xEF,0xDF,0xBF,0x7F};
+void Send(unsigned char dat)
 {
-unsigned char dat;
-while(RI==0)
+SBUF=dat;
+while(TI==0)
 ;
-RI=0;
-dat=SBUF;
-return dat;
+TI=0;
+}
+void delay(void)
+{ unsigned char m,n;
+for(m=0;m<200;m++)
+for(n=0;n<250;n++)
+;
 }
 void main(void)
 {
+unsigned char i;
 TMOD=0x20;
-SCON=0x50;
+SCON=0x40;
 PCON=0x00;
 TH1=0xfd;
 TL1=0xfd;
 TR1=1;
-REN=1;
 while(1)
 {
-P1=Receive();
+for(i=0;i<8;i++)
+{
+Send(Tab[i]);
+delay();
+}
 }
 }
