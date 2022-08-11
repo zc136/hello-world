7c7704d7b080a70d7927b5bd9e5e6f541530f252
张闯 <1362776605@qq.com>
2022-08-11 08:53:05 +0800

    Add files via upload

--- 4-6.c
+++ 4-6.c
@@ -1,40 +1,130 @@
 #include<stdio.h>
 #include<string.h>
-//char* strtok(char* str,const char* sep);
+#include<assert.h>
+//char* my_strcpy(char* arr1, const char* arr2)
+//{
+//	// (arr1 != NULL);  
+//	assert(arr1!=NULL);
+//	 //(arr2 != NULL);
+//	assert(arr1 != NULL);
+//	char* ret = arr1;
+//	while (*arr1 = *arr2)   /*  while (*arr1++=*arr2++)
+//	                                   { 
+//		                                   ;
+//	                                             }*/
+//	{
+//		arr1++;
+//		arr2++;
+//	}
+//	return ret;
+//}
 //int main()
 //{
-//char arr[] = "wwww.123@com";
-//char* p = "@.";
-//char buf[1024] = { 0 };
-//strcpy(buf, arr);
-//char* ret = strtok(arr, p);
-//printf("%s\n", ret);
-//ret=strtok(NULL, p);
-//printf("%s\n", ret);
-//ret = strtok(NULL, p);
-//printf("%s\n", ret);
-//for (ret = strtok(arr, p); ret != NULL; ret = strtok(NULL, p));
-//{
-//	printf("%s\n", ret);
+//	char arr1[] = "abcdef";
+//	char arr2[] = "biy";
+//	my_strcpy(arr1, arr2);
+//	printf("%s\n", arr1);
+//	return 0;
 //}
-//return 0;
+//char* strcpy(char* destination, const char* source);
+//char* strcat(char* destination, const char* source);
+//char* my_strcat(char* arr1, const char* arr2)
+//{
+//	// (arr1 != NULL);  
+//	assert(arr1!=NULL);
+//	 //(arr2 != NULL);
+//	assert(arr1 != NULL);
+//	char* ret = arr1;
+//	while (*arr1 != '\0')
+//	{
+//		arr1++;      //找到目的字符串的'\0'
+//	}
+//	  while (*arr1++=*arr2++)    //追加
+//	  { 
+//		  ;
+//	  }
+//	return ret;
 //}
 //int main()
 //{
-//	int arr1[10]= { 1,2,3,4,5,6,7,8,9,10 };
-//	int arr2[20] = { 0 };
-//                    
-//	
-//   	memcpy(arr2, arr1, 20);
-//	printf("%d\n", arr2);
-//		return 0;
-	int main()
-		{
-		int arr1[] = { 1,2,3,4,5,6 };
-		int arr2[] = { 1,5,4,3,2,1 };
+//	char arr1[18] = "abcdef";//保证空间充足
+//	char arr2[] = "biy";
+//	my_strcat(arr1, arr2);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
+//int my_strcmp(const char* arr1, const char* arr2)
+//{
+//	// (arr1 != NULL);  
+//	assert(arr1 != NULL);
+//	//(arr2 != NULL);
+//	assert(arr1 != NULL);
+//	while (*arr1 == *arr2)
+//	{
+//		if (*arr1 == '\0')
+//		{
+//			return 0;
+//		}
+//			arr1++;
+//			arr2++;
+//		}
+//		if (*arr1 > *arr2)
+//			return 1;
+//		else
+//			return -1;
+//	}
+//int main()
+//{
+//	char* arr1= "abcdef";
+//	char* arr2 = "biy";
+//    int ret=my_strcmp(arr1, arr2);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+//int main()
+//{
+//	char arr1[10] = "abcdefgh";
+//	char arr2[] = "biy";
+//	//strncpy 模拟实现
+//	strncpy (arr1, arr2, 2);
+//	return 0;
+//}
+//int main()
+//{
+//	char arr1[30] = "abcdefgh\0";
+//	char arr2[] = "biy";
+//	strncat (arr1, arr2, 3);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	const char*p1 = "abcdef";
+//	const char*p2 = "biy";
+//	int ret= strncmp(p1, p2, 3);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+//
 
-		int ret=	memcmp(arr1, arr2, 8);
-			printf("%d\n", ret);
-			return 0;
-		}
-//}
+int main()
+{
+	char* p1 = "abcdef";
+	char* p3 = "def";
+	char* ret = strstr(p1, p3);
+	if (ret == NULL)
+	{
+		printf("字符串不存在\n");
+	}
+	else
+	{
+		printf("%s\n",ret);
+		return 0;
+	}
+}
+
+
+
+
+
